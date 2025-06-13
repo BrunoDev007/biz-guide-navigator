34b01b350cbb7ed43c9fbdc1d5fa73d4130ad848
BrunoDev007 <bruno_xr@outlook.com>
2025-06-13 18:55:15 -0300

    mudado nome base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: '/manual-online/',
+   base: '/biz-guide-navigator/',
   server: {
     host: "::",
     port: 8080,
