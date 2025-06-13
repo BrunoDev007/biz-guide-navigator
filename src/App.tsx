227d37425e235df90104c1f577d545986a180089
BrunoDev007 <bruno_xr@outlook.com>
2025-06-13 19:07:29 -0300

    mudado basename e base

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/biz-guide-navigator">
+      <BrowserRouter basename="/manual-online">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: '/biz-guide-navigator/',
+   base: '/manual-online/',
   server: {
     host: "::",
     port: 8080,
